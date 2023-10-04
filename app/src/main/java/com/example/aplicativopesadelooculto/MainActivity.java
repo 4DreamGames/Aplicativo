de918c27e0015155c5fe5c6ccbe3d91bccb1975a
Carlos <carloseduardogomesmartins@outlook.com>
2023-10-04 10:06:45 -0300

    identificacao funcionando

--- app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.aplicativopesadelooculto;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.badge.BadgeDrawable;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.aplicativopesadelooculto.MESSAGE";
+ImageButton menuButton;
+EditText NomeDigitado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        menuButton = findViewById(R.id.identificacao);
+        NomeDigitado = findViewById(R.id.editTextNome);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                botaoClick(view);
+                String textoNome = String.valueOf(NomeDigitado.getText());
+                Intent backHome = new Intent(MainActivity.this, menu.class);
+                backHome.putExtra(EXTRA_MESSAGE, textoNome);
+                startActivity(backHome);
+            }
+        });
     }
+    public void botaoClick(View v) {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long milliseconds = 500;
+        if(vibrator != null && vibrator.hasVibrator()) {
+            vibrator.vibrate(milliseconds);
+        }
+    }
+
 }
--- app/src/main/java/com/example/aplicativopesadelooculto/menu.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/menu.java
@@ -5,20 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class menu extends AppCompatActivity {
 
+    TextView textNome;
     private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Bundle extras = getIntent().getExtras();
+        String msg = null;
+        TextView saudacao = findViewById(R.id.textNome);
+        msg = extras.getString(MainActivity.EXTRA_MESSAGE);
+        String txt = String.format("Olá <%s>",msg);
+        saudacao.setText(txt );
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
         bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.inicioItem) {
 
@@ -47,4 +54,8 @@ public class menu extends AppCompatActivity {
 
 
     }
+
+
+
+
 }
