38d38348d064e1b7d92804da00834ab715cad345
Carlos <carloseduardogomesmartins@outlook.com>
2023-10-04 08:44:27 -0300

    Estilizacao Tela Identificacao

--- app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.aplicativopesadelooculto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -10,35 +11,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView infoTextView;
-    private BottomNavigationView bottomNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        infoTextView = findViewById(R.id.infoTextView);
-
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.inicioItem) {
-                infoTextView.setText(R.string.inicio);
-            } else if (item.getItemId() == R.id.devsItem) {
-                infoTextView.setText(R.string.desenvolvedores);
-            } else if (item.getItemId() == R.id.tremItem) {
-                infoTextView.setText(R.string.historia);
-            } else if (item.getItemId() == R.id.instrucoesItem) {
-                infoTextView.setText(R.string.instrucoesGame);
-            } else if (item.getItemId() == R.id.sobreItem) {
-                infoTextView.setText(R.string.sobre);
-            }
-            return true;
-        });
-
-        BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.inicioItem);
-        badge.setNumber(7);
-        badge.setVisible(true);
     }
 }
---
+++ app/src/main/java/com/example/aplicativopesadelooculto/historia.java
@@ -0,0 +1,48 @@
+package com.example.aplicativopesadelooculto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class historia extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historia);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.inicioItem) {
+
+            } else if (item.getItemId() == R.id.devsItem) {
+
+                Intent intent = new Intent(this, devs.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.tremItem) {
+
+                Intent intent = new Intent(this, historia.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.instrucoesItem) {
+
+                Intent intent = new Intent(this, instrucoes.class);
+                startActivity(intent);
+            } else if (item.getItemId() == R.id.sobreItem) {
+
+                Intent intent = new Intent(this, sobre.class);
+                startActivity(intent);
+            }
+            return true;
+        });
+        BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.inicioItem);
+        badge.setNumber(7);
+        badge.setVisible(true);
+
+
+
+    }
+}
