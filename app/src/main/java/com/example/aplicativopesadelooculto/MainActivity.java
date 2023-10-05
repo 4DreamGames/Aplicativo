d17eee9e00a8ee7287c8ea3eb8a34f712f99b54a
MartinsCarlos <carloseduardogomesmartins@outlook.com>
2023-10-05 20:53:38 -0300

    Tratamento da tela identificacao

--- app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/MainActivity.java
@@ -1,46 +1,61 @@
 package com.example.aplicativopesadelooculto;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Vibrator;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.aplicativopesadelooculto.MESSAGE";
-ImageButton menuButton;
-EditText NomeDigitado;
+
+    public static final String EXTRA_MESSAGE = "com.example.aplicativopesadelooculto.MESSAGE";
+
+    private EditText editTextNome;
+    private ImageButton identificacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        menuButton = findViewById(R.id.identificacao);
-        NomeDigitado = findViewById(R.id.editTextNome);
-        menuButton.setOnClickListener(new View.OnClickListener() {
+
+        editTextNome = findViewById(R.id.editTextNome);
+        identificacao = findViewById(R.id.identificacao);
+
+        identificacao.setAlpha(0.5f);
+
+        editTextNome.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                botaoClick(view);
-                String textoNome = String.valueOf(NomeDigitado.getText());
-                Intent backHome = new Intent(MainActivity.this, menu.class);
-                backHome.putExtra(EXTRA_MESSAGE, textoNome);
-                startActivity(backHome);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() > 0) {
+                    identificacao.setAlpha(1.0f);  
+                } else {
+                    identificacao.setAlpha(0.5f);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        identificacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = editTextNome.getText().toString().trim();
+                if (!nome.isEmpty()) {
+                    Intent intent = new Intent(MainActivity.this, menu.class);
+                    intent.putExtra(EXTRA_MESSAGE, nome);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Por favor, digite um nome.", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
-    public void botaoClick(View v) {
-        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        long milliseconds = 500;
-        if(vibrator != null && vibrator.hasVibrator()) {
-            vibrator.vibrate(milliseconds);
-        }
-    }
-
 }
