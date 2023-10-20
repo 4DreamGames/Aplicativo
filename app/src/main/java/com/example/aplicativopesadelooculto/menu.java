0eda9f9167e8c846acdebbe2d12870d21757f8f8
LeoMasago <leo.masago@etec.sp.gov.br>
2023-10-20 11:14:45 -0300

    Iniciando - PjClain

--- app/src/main/java/com/example/aplicativopesadelooculto/menu.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/menu.java
@@ -19,7 +19,7 @@ public class menu extends AppCompatActivity {
 
     private ImageButton ImageButtonVideo1, ImageButtonVideo2;
     private ScrollView scrollViewHistoria, scrollViewInstrucoes, scrollViewSobre;
-    private TextView infoTextViewHomeIntroducao, infoTextViewDevs, infoTextViewDevs2, infoTextHistoria1, infoTextHistoria2, infoTextInstrucoes1 ,infoTextInstrucoes2, infoTextHome1, infoTextHome2, infoTextSobre1, infoTextSobre2;
+    private TextView infoTextTitulo, infoText1, infoText2, infoText3 ;
     private BottomNavigationView bottomNavigationView;
     ListView listViewDevs;
 
@@ -29,14 +29,13 @@ public class menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         ScrollView scrollViewMenu = findViewById(R.id.scrollViewMenu);
-        ScrollView scrollViewDevs = findViewById(R.id.scrollViewDevs);
         ImageButtonVideo1 = findViewById(R.id.imageVideo1);
         ImageButtonVideo2 = findViewById(R.id.imageVideo2);
 
         // Retrieve the message from the intent and display it
         Bundle extras = getIntent().getExtras();
         String msg = null;
-        TextView saudacao = findViewById(R.id.textNome);
+        TextView saudacao = findViewById(R.id.infoTextViewTitulo);
         if (extras != null && extras.containsKey(MainActivity.EXTRA_MESSAGE)) {
             msg = extras.getString(MainActivity.EXTRA_MESSAGE);
             String txt = String.format("Olá <%s>", msg);
@@ -56,83 +55,44 @@ public class menu extends AppCompatActivity {
                 startActivity(ImageButtonVideo2);
             }
         });
-        // scrollViewDevs.setVisibility(View.GONE);
-        // Initialize infoTextView and BottomNavigationView
-        infoTextHome1 = findViewById(R.id.infoTextViewHome1);
-        infoTextHome2 = findViewById(R.id.infoTextViewHome2);
-        infoTextViewHomeIntroducao = findViewById(R.id.infoTextViewHomeIntroducao);
-
-        infoTextSobre1 = findViewById(R.id.infoTextViewSobre1);
-        infoTextSobre2 = findViewById(R.id.infoTextViewSobre2);
-
-        infoTextInstrucoes1 = findViewById(R.id.infoTextViewInstru2);
-        infoTextInstrucoes2 = findViewById(R.id.infoTextViewInstru3);
-
-        infoTextHistoria1 = findViewById(R.id.infoTextViewHistoriaTitulo);
-        infoTextHistoria2 = findViewById(R.id.infoTextViewHistoria);
-
-        infoTextViewDevs2 = findViewById(R.id.infoTextViewDevs2);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        scrollViewSobre = findViewById(R.id.scrollViewSobre);
-        scrollViewInstrucoes = findViewById(R.id.scrollViewInstrucoes);
-        scrollViewHistoria = findViewById(R.id.scrollViewHistoria);
-
-
+        infoTextTitulo = findViewById(R.id.infoTextViewTitulo);
+        infoText1 = findViewById(R.id.infoTextView);
+        infoText2 = findViewById(R.id.infoTextView2);
+        infoText3 = findViewById(R.id.infoTextView3);
 
         // Set the OnItemSelectedListener for BottomNavigationView
         bottomNavigationView.setOnItemSelectedListener(item -> {
-            //esses dois codigos apagam o conteudo da pagina toda *Caso Precise*
-//            scrollViewMenu.setVisibility(View.GONE);
-//            scrollViewDevs.setVisibility(View.GONE);
             if (item.getItemId() == R.id.inicioItem) {
                 //invisible apaga o conteudo da tela dele perante a tela que vc pedir para exibir
-                scrollViewDevs.setVisibility(View.INVISIBLE);
-                scrollViewSobre.setVisibility(View.INVISIBLE);
-                scrollViewInstrucoes.setVisibility(View.INVISIBLE);
-                scrollViewHistoria.setVisibility(View.INVISIBLE);
-                infoTextViewHomeIntroducao.setText(R.string.HomeIntroducao);
-                infoTextHome1.setText(R.string.Home);
-                infoTextHome2.setText(R.string.Home);
-                scrollViewMenu.setVisibility(View.VISIBLE);
+                infoTextTitulo.setText(R.string.HomeIntroducao);
+                infoText1.setText(R.string.Home);
+                infoText2.setText(R.string.Home);
 
             } else if (item.getItemId() == R.id.devsItem) {
-                scrollViewMenu.setVisibility(View.INVISIBLE);
-                scrollViewInstrucoes.setVisibility(View.INVISIBLE);
-                scrollViewHistoria.setVisibility(View.INVISIBLE);
-                scrollViewSobre.setVisibility(View.INVISIBLE);
-                infoTextViewDevs2.setText(R.string.devTitulo);
-                infoTextViewDevs.setText(R.string.desenvolvedores2);
-                scrollViewDevs.setVisibility(View.VISIBLE);
+                infoTextTitulo.setText(R.string.devTitulo);
+                infoText1.setText(R.string.desenvolvedores2);
+                infoText2.setVisibility(View.INVISIBLE);
+                infoText3.setVisibility(View.INVISIBLE);
+                ImageButtonVideo1.setVisibility(View.INVISIBLE);
 
             } else if (item.getItemId() == R.id.tremItem) {
-                scrollViewMenu.setVisibility(View.INVISIBLE);
-                scrollViewDevs.setVisibility(View.INVISIBLE);
-                scrollViewInstrucoes.setVisibility(View.INVISIBLE);
-                scrollViewHistoria.setVisibility(View.INVISIBLE);
-                scrollViewSobre.setVisibility(View.INVISIBLE);
-                infoTextHistoria1.setText(R.string.historia);
-                infoTextHistoria2.setText(R.string.Historia);
-                scrollViewHistoria.setVisibility(View.VISIBLE);
+                infoTextTitulo.setText(R.string.HistoriaTitulo);
+                infoText2.setText(R.string.Historia);
+                ImageButtonVideo2.setVisibility(View.INVISIBLE);
+                infoText1.setVisibility(View.INVISIBLE);
+                infoText3.setVisibility(View.INVISIBLE);
 
             } else if (item.getItemId() == R.id.instrucoesItem) {
-                scrollViewMenu.setVisibility(View.INVISIBLE);
-                scrollViewDevs.setVisibility(View.INVISIBLE);
-                scrollViewHistoria.setVisibility(View.INVISIBLE);
-                scrollViewSobre.setVisibility(View.INVISIBLE);
-                infoTextInstrucoes1.setText(R.string.instrucoesGame);
-                infoTextInstrucoes2.setText(R.string.Instrucoes);
-                scrollViewInstrucoes.setVisibility(View.VISIBLE);
+                infoTextTitulo.setText(R.string.instrucoesGame);
+                infoText1.setText(R.string.Instrucoes);
 
             } else if (item.getItemId() == R.id.sobreItem) {
-                scrollViewMenu.setVisibility(View.INVISIBLE);
-                scrollViewDevs.setVisibility(View.INVISIBLE);
-                scrollViewHistoria.setVisibility(View.INVISIBLE);
-                scrollViewInstrucoes.setVisibility(View.INVISIBLE);
-                infoTextSobre1.setText(R.string.sobre);
-                infoTextSobre2.setText(R.string.conteudosobre);
-                scrollViewSobre.setVisibility(View.VISIBLE);
+                infoTextTitulo.setText(R.string.sobre);
+                infoText1.setText(R.string.conteudosobre);
+                infoText2.setVisibility(View.INVISIBLE);
+                infoText3.setVisibility(View.INVISIBLE);
             }
             return true;
         });
