84ac632fe8f565fe036c76d519d5224b79906652
Carlos <carloseduardogomesmartins@outlook.com>
2023-10-26 07:46:32 -0300

    Imagens devs arredondadas

--- app/src/main/java/com/example/aplicativopesadelooculto/ActivityCustomListView.java
+++ app/src/main/java/com/example/aplicativopesadelooculto/ActivityCustomListView.java
@@ -2,10 +2,12 @@ package com.example.aplicativopesadelooculto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import java.time.Instant;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ActivityCustomListView extends AppCompatActivity {
     @Override
